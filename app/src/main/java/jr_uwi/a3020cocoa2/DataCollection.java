3bcc0b6ffa704cf981594aebb7d198bd2dda36d4
Jason Home <jason.renwick@my.uwi.edu>
2018-04-12 09:43:11 -0400

    webview

--- app/src/main/java/jr_uwi/a3020cocoa2/DataCollection.java
+++ app/src/main/java/jr_uwi/a3020cocoa2/DataCollection.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
+import android.webkit.WebView;
 
 /**
  * Created by Jason on 9/18/2017.
@@ -15,6 +16,9 @@ public class DataCollection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.data_collection);
+        WebView simpleWebView=(WebView) findViewById(R.id.simpleWebView);
+// specify the url of the web page in loadUrl function
+        simpleWebView.loadUrl("https://www.google.com");
     }
 
 }
